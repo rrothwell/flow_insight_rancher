64fd61e4d001c78386d6c7b89ea259c45a69d637
Richard Rothwell <richard.rothwell@aarnet.edu.au>
2017-02-22 12:32:32 +1100

    Moved bash scritps used for setting up host o rancher repo.

---
+++ ansible-install-rancher/centos/centos7/prototype/prepare_elasticsearch_config.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+# Usage: ansible elasticsearch  -b -K -m script -a "/Users/developer/git/flow_insight_docker/centos/centos7/prototype/elasticsearch/prepare_elasticsearch_logs.sh "
+
+# ElasticSearch logging configuration
+# Mount to docker container using -v "$PWD/config":/usr/share/elasticsearch/config
+# ElasticSearch will merge this configuration file with the one provided by the container
+# at /usr/share/elasticsearch/config/logging.yml
+
+sudo mkdir -p /elasticsearch/config
+sudo mkdir -p /elasticsearch/config_longterm
+sudo chown 100:101 /elasticsearch/config
+sudo chown 100:101 /elasticsearch/config_longterm
+
+cat << EOF > /elasticsearch/config/logging.flowinsight.yml
+
+# you can override this using by setting a system property, for example -Des.logger.level=DEBUG
+es.logger.level: INFO
+rootLogger: \${es.logger.level}, file
+
+logger:
+  # log action execution errors for easier debugging
+  action: DEBUG
+
+appender:
+    file:
+        type: org.apache.log4j.rolling.RollingFileAppender
+        file: \${path.logs}/\${cluster.name}.log
+        rollingPolicy: org.apache.log4j.rolling.TimeBasedRollingPolicy
+        rollingPolicy.FileNamePattern: \${path.logs}/\${cluster.name}.log.%d{yyyy-MM-dd}.gz
+        layout:
+          type: pattern
+          conversionPattern: "%d{ISO8601}[%-5p][%-25c] %m%n"
+EOF
+
+# TODO RR Move this stuff to a storybook so only datanodes pick up this config.
+
+cat << EOF > /elasticsearch/config_longterm/logging.flowinsight.yml
+
+# you can override this using by setting a system property, for example -Des.logger.level=DEBUG
+es.logger.level: INFO
+rootLogger: \${es.logger.level}, file
+
+logger:
+  # log action execution errors for easier debugging
+  action: DEBUG
+
+appender:
+    file:
+        type: org.apache.log4j.rolling.RollingFileAppender
+        file: \${path.logs}/\${cluster.name}.log
+        rollingPolicy: org.apache.log4j.rolling.TimeBasedRollingPolicy
+        rollingPolicy.FileNamePattern: \${path.logs}/\${cluster.name}.log.%d{yyyy-MM-dd}.gz
+        layout:
+          type: pattern
+          conversionPattern: "%d{ISO8601}[%-5p][%-25c] %m%n"
+EOF
+
+
+# ElasticSearch short term datanode logging
+
+sudo mkdir -p /var/log/elasticsearch
+sudo chown 100:101 /var/log/elasticsearch
+
+# ElasticSearch long term datanode logging
+
+sudo mkdir -p /var/log/elasticsearch_longterm
+sudo chown 100:101 /var/log/elasticsearch_longterm
---
+++ ansible-install-rancher/centos/centos7/prototype/prepare_elasticsearch_storage.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+
+# Usage: ansible elasticsearch_baremetal  -b -K -m script -a "/Users/developer/git/flow_insight_docker/centos/centos7/prototype/elasticsearch/prepare_elasticsearch_storage.sh "
+
+# No identifiable swap partition.
+# swapoff /dev/hdb2
+
+# ElasticSearch short term datanode
+
+sudo mkdir -p /disks/ssde/elasticsearch_data
+sudo mkdir -p /disks/ssdf/elasticsearch_data
+
+sudo chown 100:101 /disks/ssde/elasticsearch_data
+sudo chown 100:101 /disks/ssdf/elasticsearch_data
+
+
+# ElasticSearch long term datanode
+
+sudo mkdir -p /disks/sdd/elasticsearch_data
+sudo mkdir -p /disks/sdc/elasticsearch_data
+sudo mkdir -p /disks/sdb10/elasticsearch_data
+sudo mkdir -p /disks/sda10/elasticsearch_data
+
+sudo chown 100:101 /disks/sdd/elasticsearch_data
+sudo chown 100:101 /disks/sdc/elasticsearch_data
+sudo chown 100:101 /disks/sdb10/elasticsearch_data
+sudo chown 100:101 /disks/sda10/elasticsearch_data
+
