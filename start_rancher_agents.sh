530e852d5c6ede8baf0679fa7ef437f2efc78860
Richard Rothwell <richard.rothwell@aarnet.edu.au>
2017-02-28 11:51:13 +1100

    Added Rancher agent management scripts to be run across the cluster using ansible.

---
+++ start_rancher_agents.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo docker run -e CATTLE_HOST_LABELS='es_node=es_data'  -d --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/rancher:/var/lib/rancher rancher/agent:v1.2.0 http://vic-crlt-gloriad4.aarnet.net.au:8080/v1/scripts/FEF02637576FDC362F96:1483142400000:poopbOTan5rlzro6ACiShOlwbU
+
