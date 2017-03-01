ac26e87d8a7bb8fbff0444946b17735e170b31b9
Richard Rothwell <richard.rothwell@aarnet.edu.au>
2017-03-02 08:47:15 +1100

    Copying run.sh now works. Added explanation in dockerentry.sh. Cleaned up the farm-conf docker file. Fixed path for template expansion in flowinsight.toml.

--- containers/flowinsight-conf/dockerentry.sh
+++ containers/flowinsight-conf/dockerentry.sh
@@ -3,14 +3,14 @@
 
 echo Copying run.sh
 
+# Wait for volume sidekick to mount /usr/local/bin.
 while [ ! -d "/usr/local/bin" ]; do
     echo Waiting for /usr/local/bin
     sleep 1
 done
 
-ls /usr/local/bin/
 cp /run.sh /usr/local/bin/
+
 echo After copying run.sh
-ls /usr/local/bin/
 
 exec /confd $@
