5f2e680f9191a5f565fd1f2f392997faf79716cd
Richard Rothwell <richard.rothwell@aarnet.edu.au>
2017-03-01 15:54:19 +1100

    Creaed Docker container insight/farm-conf to act as a configuration sidekick for the FlowInsight Rancher Stack.

--- containers/flowinsight-conf/run.sh
+++ containers/flowinsight-conf/run.sh
@@ -2,16 +2,8 @@
 
 set -e
 
-PLUGIN_TXT=${PLUGIN_TXT:-/usr/share/elasticsearch/plugins.txt}
-
-while [ ! -f "/usr/share/elasticsearch/config/elasticsearch.yml" ]; do
+while [ ! -f "/usr/local/bin/lifecycle/start_farm.sh" ]; do
     sleep 1
 done
 
-if [ -f "$PLUGIN_TXT" ]; then
-    for plugin in $(<"${PLUGIN_TXT}"); do
-        /usr/share/elasticsearch/bin/plugin --install $plugin
-    done
-fi
-
-exec /docker-entrypoint.sh elasticsearch
+exec /farm-entrypoint.sh
