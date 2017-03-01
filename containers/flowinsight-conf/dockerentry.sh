89d8148a85074842207aeeea155770b3cfa8a552
Richard Rothwell <richard.rothwell@aarnet.edu.au>
2017-03-02 08:27:33 +1100

    Added wait loop for farm-conf container to ensure /usr/local/bin is ready.

--- containers/flowinsight-conf/dockerentry.sh
+++ containers/flowinsight-conf/dockerentry.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+
 echo Copying run.sh
+
+while [ ! -d "/usr/local/bin" ]; do
+    echo Waiting for /usr/local/bin
+    sleep 1
+done
+
 ls /usr/local/bin/
 cp /run.sh /usr/local/bin/
 echo After copying run.sh
