003ea4c99fd3798a6f55e725d2d1a0bdf92b7690
Richard Rothwell <richard.rothwell@aarnet.edu.au>
2017-06-09 11:52:30 +1000

    Kibana5-conf run.sh is updated with new path for kibana.yml that is compatible with Kibana 5 base container.

--- containers/kibana5-conf/run.sh
+++ containers/kibana5-conf/run.sh
@@ -7,16 +7,19 @@
 set -e
 
 # Wait for the new kibana.yml to appear so kibana will pick the new configuration.
-while [ ! -f "/opt/kibana/config/kibana.yml.new" ]; do
-    echo "Waiting for /opt/kibana/config/kibana.yml.new"
+while [ ! -f "/usr/share/kibana/config/kibana.yml.new" ]; do
+    echo "Waiting for /usr/share/kibana/config/kibana.yml.new"
     sleep 1
 done
 
 # Backup the existing config file and put in its place the new config file.
 # We keep a copy of kibana.yml.new as it also acts as a ready flag.
 # If its not there a restart enters an infinite loop.
-mv /opt/kibana/config/kibana.yml /opt/kibana/config/kibana.yml.bak
-cp /opt/kibana/config/kibana.yml.new /opt/kibana/config/kibana.yml
+mv /usr/share/kibana/config/kibana.yml /usr/share/kibana/config/kibana.yml.bak
+cp /usr/share/kibana/config/kibana.yml.new /usr/share/kibana/config/kibana.yml
+
+echo "Config directory:"
+ls -al /usr/share/kibana/config/
 
 # Now delegate to the original entry point to finish the kibana startup process.
-exec /docker-entrypoint.sh kibana
+exec /usr/local/bin/kibana-docker
