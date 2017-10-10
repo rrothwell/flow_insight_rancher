6d2ad56977cc8163fb19266ba7c020dbc044d9d1
Richard Rothwell <richard.rothwell@aarnet.edu.au>
2017-10-10 12:07:56 +1100

    In flowinsight-reporter-conf/run.sh mv original live.conf aside so one generated by confd will be used.

--- containers/flowinsight-reporter-conf/run.sh
+++ containers/flowinsight-reporter-conf/run.sh
@@ -6,4 +6,7 @@ while [ ! -f "/usr/local/bin/lifecycle/start_reporter.sh" ]; do
     sleep 1
 done
 
+# Backup the original config file so confd can generate the replacement.
+mv /srv/insight/farm/rooster/live.config /srv/insight/farm/rooster/live.config.original
+
 exec /usr/local/bin/farm_entrypoint.sh
